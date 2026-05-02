3059ba5f29195f892d29112c646e2976accc1a3e
minhthang597259-design <minhthang597259@gmail.com>
2026-05-02 14:59:59 +0700

    Update Program.cs

--- Backend/Booking.API/Program.cs
+++ Backend/Booking.API/Program.cs
@@ -5,25 +5,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
 builder.Services.AddControllers();
 
-// Kết nối PostgreSQL
 builder.Services.AddDbContext<BookingDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Dependency Injection
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<ITicketRepository, TicketRepository>();
 builder.Services.AddScoped<IBookingService, BookingService>();
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Middleware
 app.UseSwagger();
 app.UseSwaggerUI();
 
@@ -31,7 +26,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-// Map controller
 app.MapControllers();
 
 app.Run();
