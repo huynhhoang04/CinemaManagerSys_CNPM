c27ff8298cf2accde67a5eff981e9724f83a25b8
minhthang597259-design <minhthang597259@gmail.com>
2026-05-02 14:46:29 +0700

    Add files via upload

--- Backend/Booking.API/Repositories/BookingRepository.cs
+++ Backend/Booking.API/Repositories/BookingRepository.cs
@@ -1,11 +1,38 @@
+using Booking.API.Data;
 using Booking.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Booking.API.Repositories;
 
 public class BookingRepository : IBookingRepository
 {
-    public Task<Models.Booking?> GetByIdAsync(int id) => throw new NotImplementedException();
-    public Task<IEnumerable<Models.Booking>> GetByUserIdAsync(int userId) => throw new NotImplementedException();
-    public Task AddAsync(Models.Booking booking) => throw new NotImplementedException();
-    public Task SaveChangesAsync() => throw new NotImplementedException();
+    private readonly BookingDbContext _context;
+
+    public BookingRepository(BookingDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Models.Booking?> GetByIdAsync(int id)
+    {
+        return await _context.Bookings
+            .FirstOrDefaultAsync(b => b.BookingId == id);
+    }
+
+    public async Task<IEnumerable<Models.Booking>> GetByUserIdAsync(int userId)
+    {
+        return await _context.Bookings
+            .Where(b => b.UserId == userId)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(Models.Booking booking)
+    {
+        await _context.Bookings.AddAsync(booking);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
 }
