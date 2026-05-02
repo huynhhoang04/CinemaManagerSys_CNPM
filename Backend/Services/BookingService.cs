581ba183ddc8704a22bb0d91fc97d1d2aabbb97e
minhthang597259-design <minhthang597259@gmail.com>
2026-05-02 14:41:29 +0700

    Delete Backend/Services directory

--- Backend/Services/BookingService.cs
+++
@@ -1,9 +0,0 @@
-namespace Booking.API.Services;
-
-public class BookingService : IBookingService
-{
-    public Task CreateBookingAsync() => throw new NotImplementedException();
-    public Task GetBookingByIdAsync(int id) => throw new NotImplementedException();
-    public Task GetBookingsByUserAsync(int userId) => throw new NotImplementedException();
-    public Task GetSoldSeatsByShowtimeAsync(int showtimeId) => throw new NotImplementedException();
-}
--- Backend/Services/IBookingService.cs
+++
@@ -1,9 +0,0 @@
-namespace Booking.API.Services;
-
-public interface IBookingService
-{
-    Task CreateBookingAsync();
-    Task GetBookingByIdAsync(int id);
-    Task GetBookingsByUserAsync(int userId);
-    Task GetSoldSeatsByShowtimeAsync(int showtimeId);
-}
